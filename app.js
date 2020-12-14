a599ce7cae86ece2e26316138539ba25b435e74e
skhan117 <skhan117@fiu.edu>
2020-12-13 23:59:19 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+/*
+User Management App
+CEN4010 Final Project - Reduced scope
+Written by Shaan Khan
+*/
+
+// Import Express and create an instance of it. This will be our server.
+const express = require('express');
+const app = express();
+
+// Morgan will log requests. This will help us figure out errors in code.
+const morgan = require('morgan');
+app.use(morgan('short'));
+
+// Import MySQL library for making queries to User table in our MySQL database.
+const mysql = require('mysql');
+
+// The body-parser library will be used to parse what the user enters into our html forms.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Static HTML files will be served from the public directory by Express
+app.use(express.static('./public'));
+
+// Specify root route. Use / for root. req = request, res = response.
+app.get("/", (req, res) => {
+  // Log that the root route is being touched.
+  console.log("Responding to root route");
+
+  // Here is the response to the client request to the root.
+  res.send("You have reached the root.");
+})
+
+// GetConnection is a helper function that will connect to MySQL database
+function getConnection() {
+  return mysql.createConnection({
+    host: 'localhost',        // Make connection to Heroku DB later. Local DB is fine for now. 
+    user: 'root',
+    password: '',
+    database: 'UserInfo'
+  })
+}
+
+// On a local machine the application will listen to port 8087, but when deployed the port number will be
+// set by process.env.PORT, an environmental variable provided by Heroku. 
+const myPort = 8087 || process.env.PORT;
+
+app.listen(myPort, () => {
+    console.log("Server is functional, and listening on port 8087.")
+})
+
+// Specify root route. Use / for root. req = request, res = response.
+app.get("/", (req, res) => {
+  // Log that the root route is being touched.
+  console.log("Responding to root route");
+
+  // Here is the response to the client request to the root.
+  res.send("You have reached the root.");
+})
+
+// Specify a test route. 
+app.get("/test", (req, res) => {
+  // Log that the test route is being touched.
+  console.log("Responding to test route");
+
+  // Here is the response to the client request to the root.
+  res.send("You have reached the test root.");
+})
+
